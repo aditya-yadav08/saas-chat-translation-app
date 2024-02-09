0756324766b0bca9575685909aea9b8681880563
aditya-yadav08 <aditya.yadav2708@gmail.com>
2024-02-10 03:35:55 +0530

    commit

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -1,44 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { authOptions } from "@/auth";
+import { authOptions } from "@/auth"
 import { chatMembersCollectionGroupRef } from "@/lib/converters/ChatMembers";
 import { getDocs } from "firebase/firestore";
-import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth"
 import ChatListRows from "./ChatListRows";
 
-// Define the type for your chat data
-type ChatData = {
-  timestamp: null;
-  userId: string;
-  email: string;
-  isAdmin: boolean;
-  chatId: string;
-  image: string;
-};
+async function ChatList() {
 
-function ChatList() {
-  // Initialize state with an empty array of ChatData
-  const [initialChats, setInitialChats] = useState<ChatData[]>([]);
+  const session = await getServerSession(authOptions);
 
-  useEffect(() => {
-    const fetchChats = async () => {
-      try {
-        const session = await getServerSession(authOptions);
-        const chatsSnapshot = await getDocs(chatMembersCollectionGroupRef(session?.user.id!));
-        // Map the documents to ChatData type
-        const chatsData: ChatData[] = chatsSnapshot.docs.map(doc => ({ 
-          ...doc.data(),
-          timestamp: null,
-        }));
-        setInitialChats(chatsData);
-      } catch (error) {
-        console.error("Error fetching chats:", error);
-      }
-    };
+  const chatsSnapshot = await getDocs(
+    chatMembersCollectionGroupRef(session?.user.id!)
+  );
 
-    fetchChats();
-  }, []);
+  const initialChats = chatsSnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    timestamp: null,
+  }))
 
-  return <ChatListRows initialChats={initialChats} />;
+  return <ChatListRows initialChats={initialChats} />
 }
 
 export default ChatList;
